327ce9287116df255129c700cfde0582f6b6a084
dawidplesniarski <dawidplesniarski@gmail.com>
2021-03-24 17:52:15 +0100

    Exceptions catching in test properly

--- src/main/java/com/company/BitCounter.java
+++ src/main/java/com/company/BitCounter.java
@@ -1,11 +1,20 @@
 package com.company;
 
 public class BitCounter {
-    public int noOfBits(String number) {
-        String binary = Integer.toBinaryString(Integer.parseInt(number));
+    public int noOfBits(String number) throws Exception {
 
+        if(number.isEmpty()) {
+            return 0;
+        }
+
+        int parsedNumber = Integer.parseInt(number);
+
+        if(parsedNumber > 255 || parsedNumber < 0) {
+            throw new Exception("Incorrect number");
+        }
+
+        String binary = Integer.toBinaryString(parsedNumber);
         int result = 0;
-
         for(int i = 0; i < binary.length(); i++) {
             if(binary.charAt(i) == '1') {
                 result++;
--- src/test/java/BitCountTest.java
+++ src/test/java/BitCountTest.java
@@ -2,14 +2,31 @@ import com.company.BitCounter;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BitCountTest {
     BitCounter bitCounter = new BitCounter();
 
     @Test
-    void checkNumberOfBits() {
+    void checkNumberOfBits() throws Exception {
         assertEquals(6, bitCounter.noOfBits("123"));
         assertEquals(2, bitCounter.noOfBits("132"));
         assertEquals(4, bitCounter.noOfBits("89"));
+        assertEquals(0, bitCounter.noOfBits(""));
+
+        Exception thrown = assertThrows(
+                Exception.class,
+                () -> bitCounter.noOfBits("256"),
+                "Expected doThing() to throw, but it didn't"
+        );
+
+        Exception thrownMinus = assertThrows(
+                Exception.class,
+                () -> bitCounter.noOfBits("-1"),
+                "Expected doThing() to throw, but it didn't"
+        );
+
+        assertEquals(thrown.getMessage(), "Incorrect number");
+        assertEquals(thrownMinus.getMessage(), "Incorrect number");
     }
 }
