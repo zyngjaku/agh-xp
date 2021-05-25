850436645aa0e1b0e25dcfe287f1635829148ac6
dawidplesniarski <dawidplesniarski@gmail.com>
2021-05-25 17:26:16 +0200

    getAmountTest and getDaysTest added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,25 @@
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        var repositories = new RepositoryProvider();
-        repositories.setGoalRepository(new Repository<>());
-        repositories.setCyclicalItemRepository(new Repository<>());
-        var viewSource = new ViewSource(new StdinInputSource(), repositories, new BalanceProvider());
-
-        while (true) {
-            var view = viewSource.processNextCommand();
-            if (view == null) {
-                break;
-            }
-            try {
-                view.execute();
-            }
-            catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        }
+//        var repositories = new RepositoryProvider();
+//        repositories.setGoalRepository(new Repository<>());
+//        repositories.setCyclicalItemRepository(new Repository<>());
+//        var viewSource = new ViewSource(new StdinInputSource(), repositories, new BalanceProvider());
+//
+//        while (true) {
+//            var view = viewSource.processNextCommand();
+//            if (view == null) {
+//                break;
+//            }
+//            try {
+//                view.execute();
+//            }
+//            catch (Exception e) {
+//                System.out.println(e.getMessage());
+//            }
+//        }
+        CyclicalMoneyTransfer cyclicalMoneyTransfer = new CyclicalMoneyTransfer(BigDecimal.valueOf(5000), 5);
+        System.out.println(cyclicalMoneyTransfer.getAmount());
     }
 }
