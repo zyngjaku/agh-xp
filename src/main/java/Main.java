243b1634766a5d1de06773bd4645f188c1037c0c
Kacper Tonia <32137911+marax27@users.noreply.github.com>
2021-05-26 15:27:40 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
+        var repositories = new RepositoryProvider();
+        repositories.setGoalRepository(new Repository<>());
+        repositories.setCyclicalItemRepository(new Repository<>());
+        var viewSource = new ViewSource(new StdinInputSource(), repositories, new BalanceProvider());
+
         while (true) {
             var view = viewSource.processNextCommand();
             if (view == null) {
@@ -9,7 +14,8 @@ public class Main {
             }
             try {
                 view.execute();
-            } catch (Exception e) {
+            }
+            catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
