dc3183083256cb35f769e619ca0d4bec6e8435be
marax27 <kacpert314@gmail.com>
2021-05-26 16:44:13 +0200

    Add empty classes

---
+++ src/test/java/SerializerTests.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.security.InvalidParameterException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class SerializerTests {
+
+
+
+}
