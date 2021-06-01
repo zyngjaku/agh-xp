60589cad47287ed54a5d6b339b14726ceb922492
marax27 <kacpert314@gmail.com>
2021-06-01 16:32:03 +0200

    Add JSON serializer

---
+++ src/main/java/SerializationException.java
@@ -0,0 +1,5 @@
+public class SerializationException extends Exception {
+    SerializationException(String message) {
+        super(message);
+    }
+}
--- src/test/java/SerializerTests.java
+++ src/test/java/SerializerTests.java
@@ -1,18 +1,19 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
-import java.security.InvalidParameterException;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class SerializerTests {
 
+    @Test
+    void givenValidJson_settingsContainExpectedValues() throws JsonProcessingException {
+        var objectMapper = new ObjectMapper();
+        var givenJson = "{ \"currency\": \"aaaaa\", \"language\": \"EN\" }";
+        var settings = objectMapper.readValue(givenJson, SettingsProvider.class);
 
+        assertEquals("aaaaa", settings.getCurrency());
+        assertEquals("EN", settings.getLanguage());
+    }
 
 }
