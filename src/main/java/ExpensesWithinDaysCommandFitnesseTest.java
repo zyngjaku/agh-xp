322d494062c782566a49b30f137b447b17e68c22
Dawid Karolewski <dkarolewski@student.agh.edu.com>
2021-06-04 14:20:17 +0200

    Added acceptance tests for ExpensesInMonth and ExpensesWithinDays and fix RepositoryProvider

---
+++ src/main/java/ExpensesWithinDaysCommandFitnesseTest.java
@@ -0,0 +1,22 @@
+public class ExpensesWithinDaysCommandFitnesseTest {
+    String[] dates;
+    int[] values;
+    String days;
+
+    public void setDates(String[] dates) {this.dates = dates;}
+
+    public void setValues(int[] values) {this.values = values;}
+
+    public void setDay(String days) {this.days = days;}
+
+    public String getExpensesHistory() {
+        RepositoryProvider repository  = new RepositoryProvider();
+        AddExpenseCommand cmd = new AddExpenseCommand(new BalanceProvider(), repository.getExpenseRepository());
+        for (int i=0; i<dates.length; i++) {
+            cmd.addExpense(String.format("%d %s", values[i], dates[i]));
+        }
+
+        ExpensesWithinDaysCommand expensesWithinDaysCommand = new ExpensesWithinDaysCommand(repository.getExpenseRepository());
+        return expensesWithinDaysCommand.getExpensesHistory(days).replace("\n", "");
+    }
+}
